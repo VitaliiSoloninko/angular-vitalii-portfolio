1466193cb540b4d8e2b44bcaeb23d7fb86033479
22051988Di <vitalii.soloninko@gmail.com>
2024-09-25 14:59:47 +0200

    4.4	Section Experience. Tabs

--- src/app/sections/experience/tabs/tabs.component.ts
+++ src/app/sections/experience/tabs/tabs.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './tabs.component.html',
   styleUrl: './tabs.component.scss',
 })
-export class TabsComponent {}
+export class TabsComponent {
+  tabs: any = [
+    {
+      id: 1,
+    },
+    {
+      id: 2,
+    },
+    {
+      id: 3,
+    },
+  ];
+  selectedTab: number = 1;
+  selectTab(tabId: number): void {
+    this.selectedTab = tabId;
+  }
+}
