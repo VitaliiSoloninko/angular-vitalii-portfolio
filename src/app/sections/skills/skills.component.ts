34830855ac246be34acc6147bf0a0d1f625f504c
22051988Di <vitalii.soloninko@gmail.com>
2024-10-27 14:26:19 +0100

    9.5	Add new education

--- src/app/sections/skills/skills.component.ts
+++ src/app/sections/skills/skills.component.ts
@@ -9,6 +9,42 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.scss',
 })
 export class SkillsComponent {
+  language = [
+    {
+      year: '06/2024 - 12/2024',
+      level: `German C1`,
+      institution: `WBS Training, Schwerin`,
+    },
+    {
+      year: '08/2023 - 05/2024',
+      level: `German B2`,
+      institution: `SBW Gesellschaft, Schwerin`,
+    },
+    {
+      year: '08/2023 - 05/2024',
+      level: `German B1`,
+      institution: `DAA Academy, Anklam`,
+    },
+  ];
+
+  education = [
+    {
+      year: '09/2008 - 06/2009',
+      level: `Master's degree (Civil engineer)`,
+      institution: `National university of water and environmental engineering, Rivne, Ukraine`,
+    },
+    {
+      year: '09/2004 - 06/2008',
+      level: `Bachelor's degree (Civil engineer)`,
+      institution: `National university of water and environmental engineering, Rivne, Ukraine`,
+    },
+    {
+      year: '09/1993 - 06/2004',
+      level: `Diploma's degree (High school)`,
+      institution: `High school, Kvasilov, Ukraine`,
+    },
+  ];
+
   frontendSkills = [
     'Angular',
     'TypeScript',
@@ -18,6 +54,7 @@ export class SkillsComponent {
     'Responsive Web Design',
     'Web Application',
   ];
+
   additionalSkills = [
     'Frontend Development',
     'Frontend Design',
@@ -25,6 +62,7 @@ export class SkillsComponent {
     'User Experience (UX)',
     'Git/GitHub',
   ];
+
   backendSkills = [
     'NestJS',
     'Express.js',
