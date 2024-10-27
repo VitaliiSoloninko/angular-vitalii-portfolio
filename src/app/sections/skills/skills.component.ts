2b83ac041eca4a3d442d09d9a770dbbf1ab755c9
22051988Di <vitalii.soloninko@gmail.com>
2024-10-27 13:22:28 +0100

    9.4	Add new skills

--- src/app/sections/skills/skills.component.ts
+++ src/app/sections/skills/skills.component.ts
@@ -1,12 +1,38 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrl: './skills.component.scss',
 })
 export class SkillsComponent {
-
+  frontendSkills = [
+    'Angular',
+    'TypeScript',
+    'JavaScript',
+    'CSS/SCSS',
+    'HTML',
+    'Responsive Web Design',
+    'Web Application',
+  ];
+  additionalSkills = [
+    'Frontend Development',
+    'Frontend Design',
+    'User Interface (UI)',
+    'User Experience (UX)',
+    'Git/GitHub',
+  ];
+  backendSkills = [
+    'NestJS',
+    'Express.js',
+    'REST API',
+    'MongoDB',
+    'Mongoose',
+    'PostgreSQL',
+    'Sequelize',
+    'Swagger',
+  ];
 }
