1cce049f8ef7f01474a80974c1a79f676ab4de20
22051988Di <vitalii.soloninko@gmail.com>
2024-11-01 07:50:53 +0100

    11.2	Project page (Part 1) logic (activatedRoute + service)

--- src/app/pages/project-page/project-page.component.ts
+++ src/app/pages/project-page/project-page.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Project } from '../../data/project.model';
+import { ActivatedRoute } from '@angular/router';
+import { ProjectService } from '../../services/project.service';
 
 @Component({
   selector: 'app-project-page',
   standalone: true,
   imports: [],
   templateUrl: './project-page.component.html',
-  styleUrl: './project-page.component.scss'
+  styleUrl: './project-page.component.scss',
 })
 export class ProjectPageComponent {
+  project!: Project;
 
+  constructor(activatedRoute: ActivatedRoute, projectService: ProjectService) {
+    activatedRoute.params.subscribe((params) => {
+      if (params.id) this.project = projectService.getProjectById(params.id);
+    });
+  }
 }
