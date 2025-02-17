21295a45812c61f795572dc83368138020c83e0c
22051988Di <vitalii.soloninko@gmail.com>
2025-02-17 13:53:44 +0100

    12.3	Fixing

--- src/app/data/tags.ts
+++ src/app/data/tags.ts
@@ -1,8 +1,8 @@
 import { Tag } from './tag.interface';
 
 export const tags: Tag[] = [
-  { name: 'All', count: 15 },
-  { name: 'Angular', count: 3 },
+  { name: 'All', count: 16 },
+  { name: 'Angular', count: 4 },
   { name: 'JavaScript', count: 9 },
   { name: 'Backend', count: 3 },
 ];
