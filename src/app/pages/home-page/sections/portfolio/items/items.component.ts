22ba8341ce783220c433896c72b1eb278d02ebee
22051988Di <vitalii.soloninko@gmail.com>
2025-05-24 20:05:08 +0200

    13.8	Projects. Add button show more

--- src/app/pages/home-page/sections/portfolio/items/items.component.ts
+++ src/app/pages/home-page/sections/portfolio/items/items.component.ts
@@ -1,4 +1,4 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Project } from '../../../../../data/project.model';
@@ -8,12 +8,14 @@ import { TagsComponent } from '../../../../../shared/tags/tags.component';
 @Component({
   selector: 'app-items',
   standalone: true,
-  imports: [NgFor, RouterLink, TagsComponent],
+  imports: [NgFor, NgIf, RouterLink, TagsComponent],
   templateUrl: './items.component.html',
   styleUrl: './items.component.scss',
 })
 export class ItemsComponent {
   projects: Project[] = [];
+  displayedProjects: Project[] = [];
+  showCount = 3;
 
   constructor(
     private projectService: ProjectService,
@@ -27,6 +29,13 @@ export class ItemsComponent {
       else if (params.tag)
         this.projects = this.projectService.getAllProjectsByTag(params.tag);
       else this.projects = projectService.getAll();
+
+      this.displayedProjects = this.projects.slice(0, this.showCount);
     });
+  }
+
+  showMore() {
+    this.showCount += 3;
+    this.displayedProjects = this.projects.slice(0, this.showCount);
   }
 }
