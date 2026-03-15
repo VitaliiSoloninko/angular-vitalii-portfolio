96d40fa0acaff21e19d34698ed76717c00c2d5c2
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2026-03-15 14:19:02 +0100

    refactor(routes): remove routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
-import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 import { BlogArticlePageComponent } from './pages/blog-article-page/blog-article-page.component';
 import { BlogPageComponent } from './pages/blog-page/blog-page.component';
-import { DocsPageComponent } from './pages/docs-page/docs-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ContactsComponent } from './pages/home-page/sections/contacts/contacts.component';
 import { ExperienceComponent } from './pages/home-page/sections/experience/experience.component';
@@ -17,44 +15,6 @@ export const routes: Routes = [
   { path: 'skills', component: SkillsComponent },
   { path: 'contacts', component: ContactsComponent },
   { path: 'blog', component: BlogPageComponent },
-  { path: 'admin', component: AdminPageComponent },
-  {
-    path: 'docs',
-    component: DocsPageComponent,
-    children: [
-      { path: '', redirectTo: 'introduction', pathMatch: 'full' },
-      {
-        path: 'introduction',
-        loadComponent: () =>
-          import(
-            './pages/docs-page/pages/introduction/introduction.component'
-          ).then((m) => m.IntroductionComponent),
-      },
-      {
-        path: 'authorization/jwt',
-        loadComponent: () =>
-          import(
-            './pages/docs-page/pages/authorization/jwt/jwt.component'
-          ).then((m) => m.JwtComponent),
-      },
-
-      {
-        path: 'layout/grid',
-        loadComponent: () =>
-          import('./pages/docs-page/pages/layout/grid/grid.component').then(
-            (m) => m.GridComponent
-          ),
-      },
-      {
-        path: 'layout/flexbox',
-        loadComponent: () =>
-          import(
-            './pages/docs-page/pages/layout/flexbox/flexbox.component'
-          ).then((m) => m.FlexboxComponent),
-      },
-    ],
-  },
-
   { path: 'search/:searchTerm', component: PortfolioComponent },
   { path: 'tag/:tag', component: PortfolioComponent },
   { path: 'project/:id', component: ProjectPageComponent },
--- src/app/data/tags.ts
+++ src/app/data/tags.ts
@@ -1,7 +1,7 @@
 import { Tag } from '../models/tag.interface';
 
 export const tags: Tag[] = [
-  { name: 'Alle Projekte', link: 'All' },
+  { name: 'Alle', link: 'All' },
   { name: 'Frontend', link: 'Frontend' },
   { name: 'Fullstack', link: 'Fullstack' },
   { name: 'Backend', link: 'Backend' },
