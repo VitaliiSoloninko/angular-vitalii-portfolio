b8930cf31aa7180383102db76d9ed90554272d02
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2026-03-15 14:51:56 +0100

    refactor(app): remove extra components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,25 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { AboutComponent } from './pages/home-page/sections/about/about.component';
-import { ContactsComponent } from './pages/home-page/sections/contacts/contacts.component';
-import { ExperienceComponent } from './pages/home-page/sections/experience/experience.component';
-import { PortfolioComponent } from './pages/home-page/sections/portfolio/portfolio.component';
-import { SkillsComponent } from './pages/home-page/sections/skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    AboutComponent,
-    PortfolioComponent,
-    ExperienceComponent,
-    SkillsComponent,
-    ContactsComponent,
-    FooterComponent,
-  ],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
