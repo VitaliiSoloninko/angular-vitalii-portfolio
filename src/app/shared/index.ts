4a521eed4bb27a544e2497f09079190f1741bd33
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2026-03-17 11:47:48 +0100

    refactor(project detail): change description, remove page items

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -5,7 +5,6 @@ export * from './arrow-left/arrow-left.component';
 export * from './arrow-right/arrow-right.component';
 export * from './category-badge/category-badge.component';
 export * from './nav-bar/nav-bar.component';
-export * from './page-items/page-items.component';
 export * from './search/search.component';
 export * from './skill-list/skill-list.component';
 export * from './social-links/social-links.component';
